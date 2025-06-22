23f4c885c74eb6fe9ab7f54a85751e836dadbe6c
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:05:32 +0500

    changed the 'main' branch

--- server/internal/common/database/schema/schema.go
+++ server/internal/common/database/schema/schema.go
@@ -7,4 +7,6 @@ type Users struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	UUID      string    `gorm:"unique;size:255;not null" json:"uuid"`
+	Email     string    `gorm:"unique;size:100;not null" json:"email"`
+	Password  string    `gorm:"size:255;not null" json:"password"`
 }
