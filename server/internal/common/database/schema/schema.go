c6817d361b9de97c5c999e21b770d3676f6e462a
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:06:02 +0500

    changed the 'main' branch

--- server/internal/common/database/schema/schema.go
+++ server/internal/common/database/schema/schema.go
@@ -15,4 +15,5 @@ type Chats struct {
 	ID        uint      `gorm:"primarykey" json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+	Message   string    `json:"message"`
 }
