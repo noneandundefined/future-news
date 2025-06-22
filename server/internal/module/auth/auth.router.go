3fcad5ff0a0105523760a0ca22ee9f023f0ec1d0
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:15:51 +0500

    added variable authRouter | included 'net/http', 'github.com/gorilla/mux' in auth.router.go

--- server/internal/module/auth/auth.router.go
+++ server/internal/module/auth/auth.router.go
@@ -1,1 +1,13 @@
 package auth
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func (h Handler) RegisterRoutes(router *mux.Router) {
+	authRouter := router.PathPrefix("/auth").Subrouter()
+
+	authRouter.HandleFunc("/signin", h.SigninHandler).Methods(http.MethodPost)
+}
