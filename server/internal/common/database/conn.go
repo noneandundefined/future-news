0a3d229e1da9e976d146fd7513e6f61c065322db
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:04:30 +0500

    the 'go.sum' file has been changed | added variables: loggerApp, err | added functions: ConnectDB, GetDB | if err is not equal to nil; if err is not equal to nil | the 'go.mod' file has been changed

---
+++ server/internal/common/database/conn.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"future.server/internal/common/database/schema"
+	"future.server/internal/lib"
+)
+
+var db *gorm.DB
+
+// Подключение к БД
+func ConnectDB(dsn string) {
+	loggerApp := lib.NewLogger()
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Discard,
+	})
+	if err != nil {
+		loggerApp.Error(err.Error())
+	}
+
+	err = db.AutoMigrate(&schema.Users{}, &schema.Commands{}, &schema.Devices{}, &schema.Configurations{}, &schema.Syncs{})
+	if err != nil {
+		loggerApp.Error(err.Error())
+	}
+
+	loggerApp.Info("Migration completed successfully.")
+	loggerApp.Info("Successfully connected to database!")
+}
+
+// Получения подключения к БД
+func GetDB() *gorm.DB {
+	return db
+}
