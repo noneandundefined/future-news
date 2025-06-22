f0231706d09a03f039407d6216c25872232a4f50
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:05:16 +0500

    included 'time' in schema.go | added structure Users | added type Users

--- server/internal/common/database/schema/schema.go
+++ server/internal/common/database/schema/schema.go
@@ -1,3 +1,10 @@
 package schema
 
+import "time"
 
+type Users struct {
+	ID        uint      `gorm:"primarykey" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UUID      string    `gorm:"unique;size:255;not null" json:"uuid"`
+}
