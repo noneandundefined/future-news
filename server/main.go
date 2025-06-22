9138310caf55382fdcbcfac91b5b15081ad10b33
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:01:50 +0500

    the 'go.sum' file has been changed | the 'go.mod' file has been changed | added variables: logger, db, server | added function main | err := godotenv.Load(); if err is not equal to nil; err := server.Run(); if err is not equal to nil | included 'github.com/joho/godotenv' in main.go

---
+++ server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "github.com/joho/godotenv"
+
+func main() {
+	logger := lib.NewLogger()
+
+	if err := godotenv.Load(); err != nil {
+		logger.Error(err.Error())
+	}
+
+	// Найстрока и подключение к БД
+	database.ConnectDB(os.Getenv("DSN"))
+	db := database.GetDB()
+
+	server := api.NewAPIServer(":8080", db)
+	fmt.Println("\n" + `  _    _ _______ _______ _____
+ | |  | |__   __|__   __|  __ \
+ | |__| |  | |     | |  | |__) |
+ |  __  |  | |     | |  |  ___/
+ | |  | |  | |     | |  | |
+ |_|  |_|  |_|     |_|  |_|
+
+                                `)
+
+	if err := server.Run(); err != nil {
+		logger.Error(err.Error())
+	}
+}
