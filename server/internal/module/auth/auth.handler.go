021cc712a0ba9a09dc02f204f5a20fb83dbf194e
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 00:15:20 +0500

    added function NewHandler | added structure Handler | added type Handler

--- server/internal/module/auth/auth.handler.go
+++ server/internal/module/auth/auth.handler.go
@@ -1,1 +1,14 @@
 package auth
+
+type Handler struct {
+}
+
+func NewHandler(monitor *vision.Vision, errors *packages.Errors) *Handler {
+	return &Handler{
+		monitor: monitor,
+		errors:  errors,
+	}
+}
+
+// Вход в аккаунт пользователя
+func (h Handler) SigninHandler(w http.ResponseWriter, r *http.Request) {
