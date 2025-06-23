c4fc7e9b9495da5e45d623ff7ee9ebcc71da348b
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:38:11 +0500

    added variable response | added new class module GalleryAPI | added condition logic: error instanceof AxiosError; added condition logic: error.response; added condition logic: error.request | included '@/app/config/config.client', 'axios' in gallery.api.ts | the 'package.json' file has been changed | the 'pnpm-lock.yaml' file has been changed

---
+++ client/apps/api/gallery.api.ts
@@ -0,0 +1,35 @@
+import { config } from "@/app/config/config.client";
+import axios from "axios";
+
+class GalleryAPI {
+    public async get(): Promise<Device[]> {
+		try {
+			const response = await axios.get(
+				`${
+					config.type.release == 'dev'
+						? config.links.URL_BACKEND_DEV
+						: config.links.URL_BACKEND_PROD
+				}/device`,
+				{
+					withCredentials: true,
+				}
+			);
+
+			return response.data.message;
+		} catch (error) {
+			if (error instanceof AxiosError) {
+				if (error.response) {
+					toast.error(error.response.data.message);
+				} else if (error.request) {
+					toast.error('Ошибка при отправки запроса.');
+				}
+			} else {
+				toast.error('Произошла ошибка. Пожалуйста, попробуйте снова.');
+			}
+
+			return [];
+		}
+	}
+}
+
+export default new GalleryAPI();
--- client/apps/app/config/config.client.ts
+++ client/apps/app/config/config.client.ts
@@ -1,6 +1,8 @@
 export const config = {
 	links: {
 		URL_FRONTEND_DEV: 'http://localhost:5173',
+		URL_BACKEND_DEV: 'http://localhost:5173',
+		URL_BACKEND_PROD: 'http://localhost:5173',
 	},
 	type: {
 		release: 'dev', // prod
