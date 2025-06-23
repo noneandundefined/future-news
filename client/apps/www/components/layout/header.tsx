87f667c3172bd9d112a2ed0f3bd74b006672afc8
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:16:40 +0500

    added variable Menu | included 'react' in header.tsx

--- client/apps/www/components/layout/header.tsx
+++ client/apps/www/components/layout/header.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import DotsVertical from "../icons/dots-vertical"
 
 const Header = () => {
+    const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
+
     return (
         <header className="z-10 flex justify-between items-center px-6 py-4 border-b border-[#222]">
             <div />
@@ -10,14 +13,27 @@ const Header = () => {
                     <p className="text-white">LIVE</p>
                 </div>
 
-                <div className="cursor-pointer">
+                <div className="cursor-pointer" onClick={}>
                     <DotsVertical fill="#fff" />
                 </div>
             </nav>
+
+            <Menu />
         </header>
     )
 }
 
-const 
+const Menu = () => {
+    return (
+        <>
+            <div className="absolute top-[4rem] right-[3.4rem] z-[11]">
+                <div className="bg-[#000] border border-[#374151] rounded-[6px]">
+                    <p className="text-[#fff] px-5 py-1 text-[14px] cursor-pointer hover:bg-[#222] rounded-[6px]">Gallery</p>
+                    <p className="text-[#fff] px-5 py-1 text-[14px] cursor-pointer hover:bg-[#222] rounded-[6px]">Stream</p>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default Header
