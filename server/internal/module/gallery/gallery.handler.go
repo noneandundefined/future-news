91a1cb0c469f6cfc05e6f4a990ab1c272bbfba76
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 10:55:59 +0500

    added variables: user, photo | if err is not equal to nil; if err is not equal to nil; err := actions.CreateGallery(&photo); if err is not equal to nil

--- server/internal/module/gallery/gallery.handler.go
+++ server/internal/module/gallery/gallery.handler.go
@@ -48,3 +48,36 @@ func (h Handler) SetGalleryHandler(w http.ResponseWriter, r *http.Request) {
 
 	utils.WriteJSON(w, r, http.StatusOK, "successfully adding a photo to the gallery")
 }
+
+func (h Handler) GetGalleryHandler(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("identity").(*schema.Users)
+
+	r.ParseMultipartForm(10 << 20)
+
+	file, handler, err := r.FormFile("photo")
+	if err != nil {
+		utils.WriteJSON(w, r, http.StatusBadRequest, "couldn't get the file")
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		utils.WriteJSON(w, r, http.StatusInternalServerError, "file reading error")
+		return
+	}
+
+	photo := schema.Gallery{
+		UserUUID: user.UUID,
+		Content:  fileBytes,
+		Name:     handler.Filename,
+		Format:   handler.Header.Get("Content-Type"),
+	}
+
+	if err := actions.CreateGallery(&photo); err != nil {
+		utils.WriteJSON(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	utils.WriteJSON(w, r, http.StatusOK, "successfully adding a photo to the gallery")
+}
