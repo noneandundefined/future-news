249768a56ad27940852a15d2088b5ab7a579bded
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:21:29 +0500

    included '../components/icons/plus' in gallery.tsx | added structure IconProps | added interface IconProps

--- client/apps/www/pages/gallery.tsx
+++ client/apps/www/pages/gallery.tsx
@@ -1,10 +1,11 @@
+import Plus from "../components/icons/plus"
 import IndexLayout from "../components/layout/index-layout"
 
 const Gallery = () => {
     return (
         <IndexLayout>
             <div>
-                
+                <Plus fill="#fff" />
             </div>
         </IndexLayout>
     )
