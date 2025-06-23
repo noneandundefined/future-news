f1405405d9391e0eadcecf650310b1ef7168d670
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:23:31 +0500

    changed the 'main' branch

--- client/apps/www/pages/gallery.tsx
+++ client/apps/www/pages/gallery.tsx
@@ -4,7 +4,7 @@ import IndexLayout from "../components/layout/index-layout"
 const Gallery = () => {
     return (
         <IndexLayout>
-            <div className="flex justify-center items-center border border-[#374151] rounded-[6px] p-6 min-h-[10%] max-w-[10%] hover:bg-[#111] cursor-pointer">
+            <div className="flex justify-center items-center border border-[#374151] rounded-[6px] p-6 min-h-[7rem] max-w-[10%] hover:bg-[#111] cursor-pointer">
                 <Plus fill="#fff" />
             </div>
         </IndexLayout>
