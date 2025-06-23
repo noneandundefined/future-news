972cd26a8722f9d2e532b1ab696dab36ced44806
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:27:40 +0500

    changed the 'main' branch

--- client/apps/www/pages/gallery.tsx
+++ client/apps/www/pages/gallery.tsx
@@ -19,7 +19,7 @@ const ModalAddGallery = () => {
         <>
             <div className="absolute h-screen w-screen flex flex-col justify-center items-center">
                 <div className="bg-[#000] border border-[#374151] rounded-[10px] p-6">
-                    <div>
+                    <div className="flex flex-col">
                         <p className="text-[#fff] font-medium">Add gallery</p>
                         <input type="file" />
                         <button>Save</button>
