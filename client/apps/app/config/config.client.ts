28053d61e412ece06183c84ace94b7d3dad7b0d4
Артём Власов <aworkprogramm@gmail.com>
2025-06-23 11:38:27 +0500

    changed the 'main' branch

--- client/apps/app/config/config.client.ts
+++ client/apps/app/config/config.client.ts
@@ -1,7 +1,7 @@
 export const config = {
 	links: {
 		URL_FRONTEND_DEV: 'http://localhost:5173',
-		URL_BACKEND_DEV: 'http://localhost:5173',
+		URL_BACKEND_DEV: 'http://localhost:8080',
 		URL_BACKEND_PROD: 'http://localhost:5173',
 	},
 	type: {
